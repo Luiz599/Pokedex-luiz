dcefb029732002a8cf9409544e2d56bc3a1a650e
Luiz599 <fernando2nik@gmail.com>
2025-09-15 20:40:44 -0300

    Mais merda

--- Pokedex/Data/AppDbContext.cs
+++ Pokedex/Data/AppDbContext.cs
@@ -9,15 +9,16 @@ namespace Pokedex.Data
             : base(opt)
         {
         }
-
         public DbSet<Genero> Generos { get; set; }
-
         public DbSet<Pokemon> Pokemons { get; set; }
-
         public DbSet<PokemonTipo> PokemonTipos { get; set; }
-
         public DbSet<Regiao> Regioes { get; set; }
-
         public DbSet<Tipo> Tipos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PokemonTipo>()
+                .HasKey(pt => new { pt.PokemonNumero, pt.TipoId });
+        }
     }
 }
